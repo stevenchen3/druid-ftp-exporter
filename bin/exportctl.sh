b860cd3ba015e9c8d8134648beb426ec7bff6ff3
Steven Chen <steven@alphash.io>
2017-10-24 13:14:32 +0800

    fix: update runner script

--- bin/exportctl.sh
+++ bin/exportctl.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-java -cp target/scala-2.11/druid-ftp-assembly-0.9.jar example.HttpToFtpStream $@
+java -cp target/scala-2.11/druid-ftp-assembly-0.9.jar io.alphash.RawDataExporter $@
