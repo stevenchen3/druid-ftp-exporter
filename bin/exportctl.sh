3e6be321eae7196e04b1c5557d4034f2693d2d32
Steven Chen <steven@alphash.io>
2018-08-08 15:58:23 +0800

    fix: rename assembly jar

--- bin/exportctl.sh
+++ bin/exportctl.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-
-java -cp target/scala-2.11/druid-ftp-assembly-0.9.jar io.alphash.RawDataExporter $@
+java -cp target/scala-2.11/druid-ftp-assembly-0.9.0-SNAPSHOT.jar io.alphash.RawDataExporter $@
